df153698af111224c700602b58aca7d56736f2dd
Viktor Szabo <viklukacs64@gmail.com>
2017-10-16 11:18:36 +0200

    Pokebag: Pikachu class added

---
+++ pokebag/Pikachu.java
@@ -0,0 +1,15 @@
+public class Pikachu {
+
+    String sound;
+    int HP;
+    int SP;
+
+    public Pikachu (String sound, int HP , int SP){
+
+        this.sound = "Pika-pika";
+
+
+
+
+    }
+}
--- pokebag/PokeBag.java
+++ pokebag/PokeBag.java
@@ -8,17 +8,17 @@ public class PokeBag {
         // Every pokemon has a random strength between 1 and 10.
         // Pokemons can say their sound (see above) when their Speak method is called. The Speak method should return the pokemon’s sound.
         // Create a function that returns the pokemon with the highest strength. In case of equal strengths, it’s your choice which one to return.
-        // 
+        //
         // Example:
         // pokeBag.add(new Pikachu());
         // pokeBag.add(new Pikachu());
         // pokeBag.add(new Pikachu());
         // pokeBag.add(new Bulbasaur());
         // pokeBag.add(new Charmander());
-        // 
+        //
         // System.out.println(pokeBag.get(0).speak());
         // This should print Pika-pika
-        // 
+        //
         // Pokemon stongestPokemon = pokeBag.getStrongest();
         // Should return the pokemon with the highest strength value
     }
